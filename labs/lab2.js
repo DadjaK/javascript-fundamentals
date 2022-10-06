f50473d9e1eb15972af57715b904bdad9988c1f6
Dadja <kessiedadja@gmail.com>
2022-10-06 16:29:01 -0500

    Lab 2 - My First Statements

---
+++ labs/lab2.js
@@ -0,0 +1,10 @@
+/* Lab Exercise - My First Statements */
+
+// First statement
+console.log('This is my first sentence');
+
+// Second statement
+console.log('I am new to JavaScript');
+
+// Third statement
+console.log('I am learning stuff every day');
