ab5909eb752a7142221a471fedc2c1aa825a2a29
Dadja <kessiedadja@gmail.com>
2022-10-06 15:33:59 -0500

    Lesson 2 - My First Application

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+//Single line comment
+
+/* Multi-line 
+ * Comments
+ */
+
+let variableName = 'Hello';
+
+console.log(variableName);
+
+variableName = 10;
+
+console.log(variableName);
