56f7ce0c68f66733198ec4a1a81f38c55e51c5a8
Dadja <kessiedadja@gmail.com>
2022-10-13 16:28:27 -0500

    Strings lab

---
+++ labs/stringslab.js
@@ -0,0 +1,11 @@
+/* All about Strings */
+
+// Task1
+let msg = " This statement has empty spaces ";
+console.log(msg.includes('empty'));
+console.log(msg.at(3));
+console.log(msg.trim());
+console.log(msg.toLowerCase());
+
+// Task2
+console.log("Jack and Jill went up the hill to fetch a pail of water.\nJack fell down and broke his crown and Jill came tumbling after.");
