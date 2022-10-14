df3b69c046f5e06f8233ac1b766e3c1ab54bfb52
Dadja <kessiedadja@gmail.com>
2022-10-14 01:44:48 -0500

    Numbers Lab

---
+++ labs/numberslab.js
@@ -0,0 +1,20 @@
+
+// Task1 - Numeric Separators 
+let aa = 34_038;
+let bb = 45_838.8734;
+let cc = 0b011_00010_1011;
+let dd = 0x174_C0;
+let ee = -895_612;
+
+console.log(aa);
+console.log(bb);
+console.log(cc);
+console.log(dd);
+console.log(ee);
+
+//Task2 - Number Systems
+let ex_Binary = 0b11101; // binary number
+let ex_hexadecimal = 0x1D; // hexadecimal number
+let ex_octal = 0o35; // octal number
+
+console.log(`Binary ${ex_Binary}, Hexadecimal ${ex_hexadecimal}, Octal ${ex_octal}`);
