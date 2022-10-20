289e97aa230370d6217ee12fcb0532deeb545d3c
Dadja <kessiedadja@gmail.com>
2022-10-20 16:28:18 -0500

    Operators Lab

---
+++ labs/operatorslab.js
@@ -0,0 +1,32 @@
+/* Task1 */
+function mathOrder() {
+    const total1 = 10 + 32 * 12 / 3;
+    const total2 = (10 + 32 )* 12 / 3;
+
+    console.log(`Expected Output
+    total1 ${total1}
+    total2 ${total2}`);
+} // end function
+
+mathOrder();
+
+/* Task2 */
+function Assign(num1, num2){
+    console.log(`Addition Assignment Operator ${num1 += num2}`);
+    console.log(`Multiply Assignment Operator ${num1 *= num2}`);
+    console.log(`Modulus Assignment Operator ${num1 %= num2}`);
+
+}
+
+
+
+/* Task3 */
+function compareExample(value1, value2){
+    const isSame = value1 == value2;
+    const isOtherSame = value1 === value2;
+    const isDifferent = value1 != value2;
+
+    console.log(`is same: ${isSame} is other same: ${isOtherSame} is diffferent: ${isDifferent}`);
+} // end function
+
+
