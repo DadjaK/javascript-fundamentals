24851698ab25338fd49b9b23d4559465507bf3cc
Dadja <kessiedadja@gmail.com>
2022-10-25 13:07:26 -0500

    Bottles of beer

---
+++ exercises/bottlesofbeer.js
@@ -0,0 +1,29 @@
+/* Welcome to the bottles of Beer song */
+function beerSong(){
+
+    let beerNum = 99; // local variable
+    let word ='bottles';
+
+    while(beerNum > 0){
+        if(beerNum == 1){
+            word = 'bottle';
+        } //end if
+
+        console.log(`${beerNum} ${word} of beer on the wall`)
+        console.log(`${beerNum} ${word} of beer`);
+        console.log('Take one down');
+        console.log('Pass it around');
+
+        beerNum--;
+
+        if(beerNum > 0){
+            console.log(`${beerNum} ${word} of beer on the wall.`)
+            console.log();
+        } else {
+            console.log('No more bottles of beer on the wall')
+        }
+    } // end while
+
+} // end function
+
+beerSong();
